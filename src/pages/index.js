31ec84a4923d25c25e0b6871a815837ba2919234
liqiantech <liqiantech@qq.com>
2019-07-17 18:34:16 +0800

    Add basic media query

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,24 +16,11 @@ class BlogIndex extends React.Component {
           <SEO title="Home" />
           <section className={`${styles.sectionCard} ${styles.itemOne}`}>
             <h2 className={styles.bgTitle}>百川入海</h2>
-            <h2
-              css={css`
-                font-size: 4rem;
-                color: #343434;
-              `}
-            >
-              Tech
-            </h2>
-            <p
-              css={css`
-                font-size: 1rem;
-                color: #443e51;
-                margin-top: 1rem;
-              `}
-            >
-              Some say that teaching is the best way to learn. "Teach" is a big
-              word for me, just be here to share things I learned. If you find
-              something useful here, it's my pleasure.
+            <h2 className={styles.title}>Tech</h2>
+            <p className={styles.description}>
+              I often hear people say that teaching is the best way to learn.
+              "Teach" is a big word for me, just be here to share things I
+              learned. If you find something useful here, it's my pleasure.
             </p>
           </section>
           <section className={`${styles.sectionCard} ${styles.itemTwo}`}>
