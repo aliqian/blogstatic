49ba6f28c8e869f8500349b4abc0fd882486b434
liqiantech <liqiantech@qq.com>
2019-07-20 17:31:33 +0800

    Change sidebar avatar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,11 +2,33 @@ import React, { useState } from "react"
 import { css } from "@emotion/core"
 import { rhythm } from "../utils/typography"
 import { Link } from "gatsby"
+import Image from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
 
 const green = "#dbeedd"
 const white = "#ffffff"
 
 const SideBar = ({ setTheme: _setTheme }) => {
+  const data = useStaticQuery(graphql`
+    query SideQuery {
+      logo: file(absolutePath: { regex: "/logo.png/" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      site {
+        siteMetadata {
+          author
+          social {
+            twitter
+          }
+        }
+      }
+    }
+  `)
+
   const [theme, setTheme] = useState(
     localStorage.getItem("theme-color") || white
   )
@@ -47,6 +69,17 @@ const SideBar = ({ setTheme: _setTheme }) => {
           box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
         `}
       >
+        <Image
+          fluid={data.logo.childImageSharp.fluid}
+          alt="BIG LOGO"
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            borderRadius: `100%`,
+            width: "100%",
+            height: "100%",
+          }}
+        />
         <Link
           to="/"
           css={css`
